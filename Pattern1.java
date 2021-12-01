345f3e71332a15859115f364a9ce26a5c55e9f2e
praveengitrepo1234 <praveenkumarmp309@gmail.com>
2021-12-01 10:54:04 +0530

    new change

---
+++ Pattern1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Pattern1 {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the input value");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print("  ");
+			}
+			int k=1;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print((char)(64+j)+" ");
+				k++;
+			}
+			System.out.println();
+		}
+		
+		
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
